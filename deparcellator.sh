7ae8cbfbee84ae5e7917aee5efb91f77116b1e92
Michael Hart <mgh40@cam.ac.uk>
2020-10-19 16:23:02 -0700

    Update deparcellator.sh

--- deparcellator.sh
+++ deparcellator.sh
@@ -13,13 +13,17 @@ i=1
 
 while [ ${i} -le  $numParcels ]
 do
+    
     fslmaths \
     ${path_project}/diffusion/parcellation/${parcellation}_WM_dwiSpace.nii.gz \
     -thr ${i} \
     -uthr ${i} \
     -bin \
     ${path_project}/diffusion/parcellation/${parcellation}/seeds/Seg`printf %04d $i`.nii.gz
+    
     echo ${path_project}/diffusion/parcellation/${parcellation}/seeds/Seg`printf %04d $i`.nii.gz \
+    
     >> ${path_project}/diffusion/parcellation/${parcellation}/seeds_targets_list.txt
+    
     i=$((${i}+1))
 done
